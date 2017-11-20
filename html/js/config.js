44dddf6767c66c72ee44a1fe8e97d918e22d195f
Marcus Hitchins <marcus7777@gmail.com>
2017-11-20 15:09:27 +0000

    Update config.js

--- html/js/config.js
+++ html/js/config.js
@@ -20,15 +20,6 @@
  */
 Config = function() {
     return {
-        // You will need to replace with your own Firebase configuration
-        // This can be retrieved from clicking "Add App" from the project overview page.
-        firebase: {
-            apiKey: "apiKey",
-            authDomain: "projectId.firebaseapp.com",
-            databaseURL: "https://databaseName.firebaseio.com",
-            storageBucket: "bucket.appspot.com",
-            messagingSenderId: "123456789"
-        },
         // Replace with an API key for Google Cloud Vision API
         // DO NOT DO THIS IN PRODUCTION!!! For Demo purposes only.
         visionAPI: {
